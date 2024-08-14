7e37cb656140d887144bdeae58d70ec89f38d2e9
thomassmith0526 <thomassmith0526@gmail.com>
2024-08-14 17:28:24 -0500

    userroutes

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -9,5 +9,17 @@ module.exports ={
         } catch (err) {
             res.status(500).json(err)
         }
-    }
+    },
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({_id:req.params.thoughtId})
+            .populate('user')
+
+            if(!thought) {
+                return res.status(404).json({ message: 'No thought with that Id'})
+            }
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
 }
---
+++ controllers/usercontrollers.js
@@ -0,0 +1,66 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find()
+                .populate('thoughts');
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.thoughtId })
+                .populate('thoughts')
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that Id' })
+            }
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async deleteUser(req, res) {
+        try{
+            const user = await User.findOneAndDelete({ _id: req.params.userId})
+
+            if(!user) {
+                return res.status(404).json({ message: 'No user with that Id'})
+            }
+
+            await Thought.deleteMany({ _id: {$in: user.thought }});
+            res.json({ message: 'User and Thoughts are deleted'})
+        }catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId},
+                { $set: req.body},
+                {runValidator: true, new: true}
+            );
+        if(!user) {
+            res.status(404).json({message: 'no user with that Id'})
+        }
+        
+        res.json(user);
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    }, 
+
+};
