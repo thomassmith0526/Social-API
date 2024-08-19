1aab261922c6196318045fb9abb8a5802d429473
thomassmith0526 <thomassmith0526@gmail.com>
2024-08-19 17:31:04 -0500

    seeds

--- utils/seed.js
+++ utils/seed.js
@@ -7,5 +7,29 @@ connection.on('error', (err) => err);
 
 connection.once('open', async () => {
     let thoughtCheck = await connection.db.listCollections({ name: 'thoughts'}).toArray()
-})
+    if (thoughtCheck.length) {
+        await connection.dropCollection('thoughts');
+    }
+
+    let userCheck = await connection.db.listCollections({ name: 'users'}).toArray();
+    if (userCheck.length) {
+        await connection.dropCollection('users');
+    }
+
+    const users = [];
+
+    for (let i = 0; i < 20; i++) {
+        const username = getRandomUsername()
+        const email = getRandomEmail()
+
+        users.push({
+            username,
+            email,
+        })
+    }
+
+    await User.insertMany(users);
+    console.log(users)
+    process.exit(0);
+});
   
